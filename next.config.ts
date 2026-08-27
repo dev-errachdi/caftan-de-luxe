dde3e625a42f8807e470a4d47a3ac7418ae093eb
dev-errachdi <mohamad2011errachdi@gmail.com>
2026-08-27 15:00:08 +0100

    ⚡ Configure project for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  experimental: {
-    optimizeCss: false,
+    unoptimized: true,
   },
 };
 
